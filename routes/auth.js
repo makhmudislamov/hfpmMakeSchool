3d53afb0caa061a77bfd758c3a8a9e61a004a91d
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-01-10 21:20:21 -0800

    route for auth is done

--- routes/auth.js
+++ routes/auth.js
@@ -54,6 +54,37 @@ module.exports = function (app) {
             err.status = 400;
             return next(err);
         }
+    });
 
+    // GET route after registering
+    app.get('/profile', function (req, res, next) {
+        User.findById(req.session.userId)
+            .exec(function (error, user) {
+                if (error) {
+                    return next(error);
+                } else {
+                    if (user === null) {
+                        var err = new Error('Not authorized! Go back!');
+                        err.status = 400;
+                        return next(err);
+                    } else {
+                        return res.send('<h1>Name: </h1>' + user.username + '<h2>Mail: </h2>' + user.email + '<br><a type="button" href="/logout">Logout</a>')
+                    }
+                }
+            });
+    });
+
+    // GET for logout logout
+    router.get('/logout', function (req, res, next) {
+        if (req.session) {
+            // delete session object
+            req.session.destroy(function (err) {
+                if (err) {
+                    return next(err);
+                } else {
+                    return res.redirect('/');
+                }
+            });
+        }
     });
 };
