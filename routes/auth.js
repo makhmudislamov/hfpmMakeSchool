dccc38eb9b5a004eec9f43b54e19026b4e9b67fe
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-01-10 21:14:41 -0800

    user creation

--- routes/auth.js
+++ routes/auth.js
@@ -8,17 +8,52 @@ module.exports = function (app) {
     });
 
     // CREATE a user
-    app.post("/sign-up", (req, res) => {
-        // Create User
-        const user = new User(req.body);
+    app.post('/sign-up', (req, res) => {
+        // confirm that user typed same password twice
+        if (req.body.password !== req.body.passwordConf) {
+            var err = new Error('Passwords do not match.');
+            err.status = 400;
+            res.send("passwords dont match");
+            return next(err);
+        }
 
-        user
-            .save()
-            .then(user => {
-                res.redirect("/");
-            })
-            .catch(err => {
-                console.log(err.message);
+        if (req.body.email &&
+            req.body.username &&
+            req.body.password &&
+            req.body.passwordConf) {
+
+            var userData = {
+                email: req.body.email,
+                username: req.body.username,
+                password: req.body.password,
+                passwordConf: req.body.passwordConf,
+            }
+
+            User.create(userData, function (error, user) {
+                if (error) {
+                    return next(error);
+                } else {
+                    req.session.userId = user._id;
+                    return res.redirect('/profile');
+                }
             });
+
+        } else if (req.body.logemail && req.body.logpassword) {
+            User.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
+                if (error || !user) {
+                    var err = new Error('Wrong email or password.');
+                    err.status = 401;
+                    return next(err);
+                } else {
+                    req.session.userId = user._id;
+                    return res.redirect('/profile');
+                }
+            });
+        } else {
+            var err = new Error('All fields required.');
+            err.status = 400;
+            return next(err);
+        }
+
     });
 };
